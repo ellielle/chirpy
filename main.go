42a0766a011ee61937b392a1ac874cfc2bf34095
ellielle <40385743+ellielle@users.noreply.github.com>
2024-02-21 15:24:15 -0500

    add handlers for routes

--- main.go
+++ main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const port = "8080"
+	apiCfg := &apiConfig{
+		fileserverHits: 0,
+	}
 	mux := http.NewServeMux()
+	// wrap mux in CORS headers
 	corsMux := middlewareCors(mux)
-	server := http.Server{
-		Addr:    "http://localhost:8080",
+	fileseverHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middelwareMetricsInc(fileseverHandler))
+	//mux.Handle("/app/assets", http.StripPrefix("/app", http.FileServer(http.Dir("./assets"))))
+	mux.HandleFunc("/healthz", healthzResponseHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsResponseHandler)
+
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: corsMux,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
+func healthzResponseHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
 
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+// FIXME: unused, and getMetricsData is probably unnecessary
+func (cfg *apiConfig) metricsResponseHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	//w.Write([]byte())
 }
