7ed0f2226955bbc35d5d4c71dddde78fa0a56fc5
ellielle <40385743+ellielle@users.noreply.github.com>
2024-02-28 14:44:59 -0500

    add profanity filter

--- validation.go
+++ validation.go
@@ -20,8 +20,6 @@ func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
 		CleanedBody string `json:"cleaned_body"`
 	}
 
-	theProfane := []string{"kerfuffle", "sharbert", "fornax"}
-
 	// Create a new JSON decoder and check the validity of the JSON from the Request body
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -36,28 +34,31 @@ func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Checks for profanity usage by looping over theProfane slice and checking the words against a lower cased copy of params.Body
-	// FIXME: lower casing the string is causing the original to be borked
-	// redo algorithm
-	tempStr := strings.ToLower(params.Body)
-	hasProfanity := false
+	cleanedBody, hasProfanity := validateNoProfanity(params.Body)
+
+	if hasProfanity {
+		respondWithJSON(w, 200, returnCleaned{CleanedBody: cleanedBody})
+		return
+	}
+	respondWithJSON(w, 200, returnCleaned{CleanedBody: params.Body})
+}
+
+func validateNoProfanity(bodyText string) (cleanedText string, hasProfanity bool) {
+	// Checks for profanity usage by looping over theProfane slice and checking the words against a lower cased params.Body
+	theProfane := []string{"kerfuffle", "sharbert", "fornax"}
+
+	splitStr := strings.Split(bodyText, " ")
+	hasProfanity = false
 	for _, profanity := range theProfane {
-		if strings.Contains(tempStr, profanity) {
-			splitStr := strings.Split(tempStr, " ")
+		if strings.Contains(strings.ToLower(bodyText), strings.ToLower(profanity)) {
 			for j, str := range splitStr {
-				if str == profanity {
+				if strings.EqualFold(str, profanity) {
 					splitStr[j] = "****"
 				}
 			}
-			tempStr = strings.Join(splitStr, " ")
-			//strings.ReplaceAll(params.Body, profanity, "****")
+			cleanedText = strings.Join(splitStr, " ")
 			hasProfanity = true
 		}
 	}
-
-	if hasProfanity {
-		respondWithJSON(w, 200, returnCleaned{CleanedBody: tempStr})
-		return
-	}
-	respondWithJSON(w, 200, returnValid{Valid: true})
+	return cleanedText, hasProfanity
 }
