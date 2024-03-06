09363ef47ff5542c2ec7af9b7f7919cdd85bd8c7
ellielle <40385743+ellielle@users.noreply.github.com>
2024-03-06 10:10:26 -0500

    add route for creating a user
    add cli flag for debug mode to delete test database on launch

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+
+	database "github.com/ellielle/chirpy/internal/database"
 )
 
 func main() {
@@ -15,6 +18,17 @@ func main() {
 	r := chi.NewRouter()
 	// Wrap r in CORS headers
 	corsMux := middlewareCors(r)
+
+	// Wipe test database in debug mode
+	dbg := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+	if *dbg {
+		err := database.DebugWipeTestDatabase("./database.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print("Database deleted successfully...")
+	}
 
 	// Fileserver for handling static pages
 	fileseverHandler := apiCfg.middelwareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
@@ -32,12 +46,14 @@ func main() {
 		r.Get("/healthz", healthzResponseHandler)
 		// Page hit count reset endpoint
 		r.HandleFunc("/reset", apiCfg.metricsResetHandler)
-		// POST endpoint to submit "Chirps". Chrips must be 140 chars or less, and should be in JSON
-		r.Post("/chirps", validateChirpHandler)
 		// GET endpoint for retrieving all chirps
 		r.Get("/chirps", getChirpsHandler)
 		// GET endpoint for retrieving a single chirp
 		r.Get("/chirps/{chirpID}", getSingleChirpHandler)
+		// POST endpoint to submit "Chirps". Chrips must be 140 chars or less, and should be in JSON
+		r.Post("/chirps", validateChirpHandler)
+		// POST endpoint to submit an email and create a new User
+		r.Post("/users", validateUserHandler)
 	})
 
 	server := &http.Server{
