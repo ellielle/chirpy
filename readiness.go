527f21ff05b3fdbeb161250503af0e7f212e7b5a
ellielle <40385743+ellielle@users.noreply.github.com>
2024-03-07 08:53:26 -0500

    add defer body close

--- readiness.go
+++ readiness.go
@@ -3,7 +3,8 @@ package main
 import "net/http"
 
 // Responds to the /api/healthz endpoint with readiness indication
-func healthzResponseHandler(w http.ResponseWriter, _ *http.Request) {
+func healthzResponseHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
