f97c8baca5819320a94877a96f6f15880900e01c
ellielle <40385743+ellielle@users.noreply.github.com>
2024-03-07 13:27:53 -0500

    add user validation

--- internal/database/users.go
+++ internal/database/users.go
@@ -1,22 +1,40 @@
 package database
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	Id    int    `json:"id"`
-	Email string `json:"email"`
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
+var ErrInvalidLogin = errors.New("invalid login")
+
 // Creates a new User and saves it to disk
-func (db *DB) CreateUser(body string) (User, error) {
+func (db *DB) CreateUser(email, password string) (User, error) {
 	dbStructure, err := db.loadDB()
 	if err != nil {
 		return User{}, err
 	}
 
+	_, err = getUserByEmail(email, &dbStructure)
+	if err == nil {
+		return User{}, errors.New("username taken")
+	}
+
+	// hash password with bcrypt
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+
 	// Create a new User with the next incremental ID
 	nextID := len(dbStructure.Users) + 1
 	user := User{
-		Id:    nextID,
-		Email: body,
+		Id:       nextID,
+		Email:    email,
+		Password: string(hash),
 	}
 	dbStructure.Users[nextID] = user
 	err = db.writeDB(dbStructure)
@@ -25,4 +43,42 @@ func (db *DB) CreateUser(body string) (User, error) {
 	}
 
 	return user, nil
+}
+
+func (db *DB) LoginUser(email, password string) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	foundUser, err := getUserByEmail(email, &dbStructure)
+	if err != nil {
+		return User{}, err
+	}
+
+	// Compare hashes and return a valid response if they match
+	// Return invalid login error inf mismatched
+	err = bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(password))
+	if err != nil {
+		return User{}, ErrInvalidLogin
+	}
+
+	return foundUser, nil
+}
+
+func getUserByEmail(email string, dbStructure *DBStructure) (User, error) {
+	// Find User in database, and return it
+	// Will return an error if the user does not exist
+	foundUser := User{}
+	for _, user := range dbStructure.Users {
+		if user.Email == email {
+			foundUser = user
+			break
+		}
+	}
+	if foundUser == (User{}) {
+		return User{}, ErrInvalidLogin
+	}
+
+	return foundUser, nil
 }
--- users.go
+++ users.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strings"
 )
 
 type User struct {
-	Email string
-	Id    int
+	Id    int    `json:"id"`
+	Email string `json:"email"`
 }
+
+var ErrInvalidPassword = errors.New("password missing or invalid")
+var ErrInvalidEmail = errors.New("email is invalid")
 
 func (cfg apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -26,24 +31,65 @@ func (cfg apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) 
 		respondWithError(w, http.StatusBadRequest, "Bad Request")
 		return
 	}
-	// No password validation other than existence
-	if params.Password == "" {
-		respondWithError(w, http.StatusUnauthorized, "No password given")
+
+	// Ensure User's email and password are valid
+	err = validateUser(params.Email, params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
 	// Create a new user with the body and save it to database in a new goroutine
-	user, err := cfg.DB.CreateUser(params.Email)
+	user, err := cfg.DB.CreateUser(params.Email, params.Password)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJSON(w, http.StatusCreated, user)
+	respondWithJSON(w, http.StatusCreated, User{Id: user.Id, Email: user.Email})
 }
 
 func (cfg apiConfig) handlerUsersLogin(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	respondWithError(w, 500, "Method not finished")
-	return
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Create a new JSON decoder and check the validity of the JSON from the Request body
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	// Ensure User's email and password are valid
+	err = validateUser(params.Email, params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// Attempt to log user in, a failture will result in a 401 not authorized error
+	user, err := cfg.DB.LoginUser(params.Email, params.Password)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, User{Id: user.Id, Email: user.Email})
+}
+
+func validateUser(email, password string) error {
+	// No password validation other than existence
+	if password == "" {
+		return ErrInvalidPassword
+	}
+	// Most minimum of requirements for an email
+	if !strings.Contains(email, "@") {
+		return ErrInvalidEmail
+	}
+	return nil
 }
